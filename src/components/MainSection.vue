a1d626ca238d45d690a81f79a0ee915b81203bcc
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-07-26 16:41:44 +0200

    creato array di 10 oggetti su vue

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -10,10 +10,19 @@ import axios from 'axios';
 
 export default {
 name:"MainSection",
+data(){
+    return {
+        albums: []
+    }
+
+    
+},
 created(){
     axios.get("https://flynn.boolean.careers/exercises/api/array/music")
     .then((response) => {
         console.log(response.data)
+
+        this.albums = response.data.response
     })
 }
 }
